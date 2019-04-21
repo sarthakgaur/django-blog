334315079903ffed56947c164c0f81616a55abf2
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2019-04-21 17:33:55 +0530

    Changes static_root cariable in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -143,7 +143,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static')
     )
